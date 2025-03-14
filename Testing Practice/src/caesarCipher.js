21365517df73982132e6710ef23edde4a545a800
Andy Wu <wuandy6418@gmail.com>
2025-03-14 15:09:48 -0400

    completed caesar cipher test and function

--- Testing Practice/src/caesarCipher.js
+++ Testing Practice/src/caesarCipher.js
@@ -1,15 +1,19 @@
-export function caesarCipher(string, number) {
+export function caesarCipher(string, number = 1) {
     let res = "";
     for (let i = 0; i < string.length; i++){
-        const charCode = char.charCodeAt(string[i]);
+        const charCode = string.charCodeAt(i);
         if (charCode >= 97 && charCode <= 122) {
+            // Lowercase letters (a-z)
             res += String.fromCharCode(((charCode - 97 + number) % 26) + 97);
         }
-        if (charCode >= 65 && charCode <= 90) {
+        else if (charCode >= 65 && charCode <= 90) {
+            // Uppercase letters (A-Z)
             res += String.fromCharCode(((charCode - 65 + number) % 26) + 65);
         }
         else {
-            res += String.fromCharCode(charCode);
+            // Non-alphabetic characters
+            res += string[i];
         }
     }
+    return res;
 }
--- Testing Practice/test/casearCipher.test.js
+++ Testing Practice/test/casearCipher.test.js
@@ -2,5 +2,5 @@ import { caesarCipher } from "../src/caesarCipher";
 
 test("caesar cipher of x number", () => {
     expect(caesarCipher("abc!", 3)).toBe("def!");
-    expect(caesarCipher("XYZ ")).toBe("ABC ");
+    expect(caesarCipher("XYZ ", 3)).toBe("ABC ");
 })
