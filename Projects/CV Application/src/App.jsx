8c705d3742c1eac0360f76bf6ae73c3141b8f29e
Andy Wu <wuandy6418@gmail.com>
2025-04-02 16:32:26 -0400

    initialize CV App  with Vite + React application. seperating app with 3 componenets = general, education, experience

---
+++ Projects/CV Application/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import General from './General'
+import Education from './Education'
+import Experience from './Experience'
+
+function App() {
+
+  return (
+    <>
+      <General></General>
+      <Education></Education>
+      <Experience></Experience>
+    </>
+  )
+}
+
+export default App
